e5182af1fbedd0481d8d13124ecd641580f1ecd1
Alexei Gousev <numegil@numegil-mbp1.local>
2016-02-15 22:39:52 -0800

    First view

--- chooser/views.py
+++ chooser/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from random import choice
+
+from chooser.models import Game
+
+def get_random_game(request):
+    num_players = request.GET['num_players']
+
+    games = Game.objects.filter(
+        min_players__gte=num_players,
+        max_players__lte=num_players,
+    )
+
+    if len(games) == 0:
+        return HttpResponse('No games found for ' + num_players + ' players.')
+
+    game = choice(games)
+
+    return HttpResponse('You are playing: ' + game.name)
