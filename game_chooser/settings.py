3f2a47ee6b744506344a4e0dc1f6785b333dd797
Alexei Gousev <numegil@fb.com>
2016-07-07 17:24:46 -0700

    flatpages template

--- game_chooser/settings.py
+++ game_chooser/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'game_chooser.urls'
 TEMPLATES = (
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+          os.path.join(PROJECT_ROOT, '../templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
