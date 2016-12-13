70d6f63f2e7fe2d4e5796848705c42704263e777
Alexei Gousev <numegil@fb.com>
2016-12-12 18:45:25 -0800

    import

--- chooser/views.py
+++ chooser/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from datetime import datetime, timedelta
 from random import choice, randint, seed
